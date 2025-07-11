565a1bde8ffb3b410fca51876755e0bcba319376
Rahul <rathavarahul870@gmail.com>
2025-07-11 16:13:43 +0530

    first commit

--- exchange_api.py
+++ exchange_api.py
@@ -3,14 +3,18 @@ import requests
 def get_price_data(asset):
     try:
         symbol = f"{asset}-USDT"
-        url = f"https://www.okx.com/api/v5/market/ticker?instId={symbol}"
-        response = requests.get(url).json()
-        spot_price = float(response["data"][0]["last"])
-        
-        futures_url = f"https://www.okx.com/api/v5/market/ticker?instId={symbol}-SWAP"
-        response2 = requests.get(futures_url).json()
-        perp_price = float(response2["data"][0]["last"])
-        
+        spot_url = f"https://okx.com/api/v5/market/ticker?instId={symbol}"
+        spot_response = requests.get(spot_url).json() 
+        spot_price = float(spot_response["data"][0]["last"])
+
+    # Fetching perpetual prices
+        perp_url = f"https://okx.com/api/v5/market/ticker?instId={symbol}-SWAP"
+        perp_response = requests.get(spot_url).json()
+        perp_price = float(spot_response["data"][0]["last"])
+
         return {"spot": spot_price, "perp": perp_price}
-    except:
-        return {"spot": 0.0, "perp": 0.0}
+    except Exception as e:
+        print(f"Error fetching prices for {asset}: {e}")
+        return {"spot": 30000, "perp": 30500}
+        
+        
